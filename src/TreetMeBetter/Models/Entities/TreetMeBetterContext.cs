f6b0b41ab2d33f8ca19dcb2865093a9742a12d7d
Joel Jönsson <joel@webehome.com>
2016-12-01 12:24:36 +0100

    Removed Connection string..

--- src/TreetMeBetter/Models/Entities/TreetMeBetterContext.cs
+++ src/TreetMeBetter/Models/Entities/TreetMeBetterContext.cs
@@ -9,7 +9,7 @@ namespace TreetMeBetter.Models.Entities
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-            optionsBuilder.UseSqlServer(@"Server=tcp:treetmebetter.database.windows.net,1433;Initial Catalog=TreetMeBetter;Persist Security Info=False;User ID=dbadmin;Password=Paulinepassword1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30");
+            optionsBuilder.UseSqlServer(@"");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
